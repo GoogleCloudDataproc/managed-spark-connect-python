975066df981d0d9db36384b48bd6bfee26f55ab5
Tim Utegenov <tima1000@gmail.com>
2026-01-29 19:23:07 -0800

    Fix broken test

--- tests/unit/test_pypi_artifacts.py
+++ tests/unit/test_pypi_artifacts.py
@@ -26,7 +26,7 @@ class PyPiArtifactsTest(unittest.TestCase):
     def test_bad_format(self):
         with self.assertRaisesRegex(
             InvalidRequirement,
-            "Expected end or semicolon \(after name and no valid version specifier\).*",
+            "Expected semicolon \(after name and no valid version specifier\).*",
         ):
             PyPiArtifacts({"pypi://spacy:23"})
 
